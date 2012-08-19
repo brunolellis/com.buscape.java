e95ae8fbdbb036f4b903d0ba67024e1dd50b3499
Bruno Lellis <brunolellis@gmail.com>
2012-08-18 22:03:08 -0300

    heroku

--- src/main/java/br/com/brunolellis/cliente/web/BuscapeWeb.java
+++ src/main/java/br/com/brunolellis/cliente/web/BuscapeWeb.java
@@ -1,6 +1,7 @@
 package br.com.brunolellis.cliente.web;
 
 import java.io.IOException;
+import java.util.List;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
@@ -11,15 +12,50 @@ import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.servlet.ServletContextHandler;
 import org.eclipse.jetty.servlet.ServletHolder;
 
+import br.com.brunolellis.cliente.local.BuscapeCliente;
+
+import com.buscape.java.api.response.Offer;
+
 public class BuscapeWeb extends HttpServlet {
 
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		
-		resp.getWriter().print("Hello from Java!\n");
+		resp.getWriter().print("<html><head><title>Preços</title></head><body>");
+		
+		BuscapeCliente cliente = new BuscapeCliente();
+		
+		int ipad = 385727; // novo ipad 32gb wifi
+		int nespresso = 86753;
+		int dolceGusto = 298833;
+		int samsungsIII = 397366;
+		
+		resp.getWriter().print("<pre>");
+		
+		resp.getWriter().println("*** iPad:");
+		exibirOfertas(cliente.pesquisarOfertas(ipad), resp);
+		resp.getWriter().println("<hr/>");
+		resp.getWriter().println("*** Nespresso:");
+		exibirOfertas(cliente.pesquisarOfertas(nespresso), resp);
+		resp.getWriter().println("<hr/>");
+		resp.getWriter().println("*** Dolce Gusto:");
+		exibirOfertas(cliente.pesquisarOfertas(dolceGusto), resp);
+		
+		resp.getWriter().print("</pre></body></html>");
 		
 	}
+	
+	private void exibirOfertas(List<Offer> offers, HttpServletResponse resp) throws IOException {
+		if (offers != null) {
+			for (Offer offer : offers) {
+				resp.getWriter().println(String.format("%d) %s = R$ %.2f", offer.getId(), offer.getSeller().getSellerName(), offer.getPrice().getValue()));
+				
+			}
 
+		}		
+		
+	}
+	
 	public static void main(String[] args) throws Exception {
 		Server server = new Server(Integer.valueOf(System.getenv("PORT")));
 		ServletContextHandler context = new ServletContextHandler(ServletContextHandler.SESSIONS);
