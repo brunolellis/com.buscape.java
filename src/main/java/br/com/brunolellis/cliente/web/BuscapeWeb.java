ca433b358b1edde43731a35aa829644fd7c0a2e4
Bruno Lellis <brunolellis@gmail.com>
2016-09-28 20:12:09 -0300

    monitor lg

--- src/main/java/br/com/brunolellis/cliente/web/BuscapeWeb.java
+++ src/main/java/br/com/brunolellis/cliente/web/BuscapeWeb.java
@@ -34,9 +34,8 @@ public class BuscapeWeb extends HttpServlet {
 		
 		//exibirOfertas(606174, "Lenovo Z40-70 Intel Core i7-4500U 1.8 GHz 8192 MB 1024 GB", resp);
 		//exibirOfertas(611724, "Lenovo Z40-70 Intel Core i7-4500U 1.8 GHz 16384 MB 1024 GB", resp);
-		exibirOfertas(1857608670, "Piano Para Leigos - Acompanha CD - Blake Neely", resp);
+		exibirOfertas(598166, "LG 23MP55HQ LED IPS 23.0 polegadas", resp);
 		
-		exibirOfertas(1858041339, "Box Quarteto de Noivas - Nora Roberts", resp);
 		
 		resp.getWriter().print("</pre></body></html>");
 		
