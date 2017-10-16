575cb64efb2035a78033dd904d3346a092f0622e
Bruno Lellis <brunolellis@gmail.com>
2017-10-16 11:14:16 -0200

    Philco Ciclone Force 1000 W

--- src/main/java/br/com/brunolellis/cliente/web/BuscapeWeb.java
+++ src/main/java/br/com/brunolellis/cliente/web/BuscapeWeb.java
@@ -40,6 +40,8 @@ public class BuscapeWeb extends HttpServlet {
 		
 		//exibirOfertas(619795, "Smartphone Lenovo Vibe K5", resp);
 		
+		
+		exibirOfertas(635368, "Philco Ciclone Force 1000 W", resp);
 		exibirOfertas(620348, "Philco PH Rapid 1000 W", resp);
 		exibirOfertas(616718, "Brastemp BAA60EB 60 cm Parede", resp);
 		
